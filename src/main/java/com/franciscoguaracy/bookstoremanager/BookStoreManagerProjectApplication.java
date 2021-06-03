14a5be07d4d7cf98fc2008631863c0be9d92e64c
FranciscoGuaracy <guaracykyokushin@gmail.com>
2021-06-03 19:44:17 -0300

    First commit

---
+++ src/main/java/com/franciscoguaracy/bookstoremanager/BookStoreManagerProjectApplication.java
@@ -0,0 +1,14 @@
+package com.franciscoguaracy.bookstoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreManagerProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BookStoreManagerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franciscoguaracy/bookstoremanager/BookStoreManagerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franciscoguaracy.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreManagerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
